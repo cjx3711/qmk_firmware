fdf0dbe3631552ba16c6559bdb786b9e411373b8
Jia Xun <chaijiaxun@gmail.com>
2021-02-05 01:22:20 +0800

    Updated macropad for v2

--- keyboards/lifeclocc/artist_macropad/artist_macropad.h
+++ keyboards/lifeclocc/artist_macropad/artist_macropad.h
@@ -27,11 +27,11 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    k00, k01, k02, k03, \
-    k10, k11, k12, k13, \
-     k20, k21,   k22 \
+    k00, k01, k02, k03, k04, \
+    k10, k11, k12, k13, k14, \
+     k20,  k21, k22,    k23 \
 ) { \
-    { k00, k01, k02, k03 }, \
-    { k10, k11, k12, k13 }, \
-    { k20, k21, k22, KC_NO } \
+    { k00, k01, k02, k03, k04 }, \
+    { k10, k11, k12, k13, k14 }, \
+    { k20, k21, k22, k23, KC_NO } \
 }
--- keyboards/lifeclocc/artist_macropad/keymaps/default/keymap.c
+++ keyboards/lifeclocc/artist_macropad/keymaps/default/keymap.c
@@ -14,11 +14,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#define xxx KC_NO
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+    _UPPER,
+    _MID,
+    _LOWER,
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -29,15 +31,21 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT(
-        C(KC_EQUAL), KC_RBRC, C(KC_Z), KC_B, \
-        C(KC_MINS), KC_LBRC, C(KC_D), KC_E, \
-        LT(1, KC_ESC), LALT_T(KC_V), KC_SPC
+    [_UPPER] = LAYOUT(
+        C(KC_C), KC_RBRC, C(KC_J), KC_E, KC_B, \
+        C(KC_V), KC_LBRC, LALT_T(KC_X), C(KC_D), C(KC_Z), \
+        LCTL_T(KC_ESC), LT(1, KC_BSPC), LT(_LOWER, KC_V), KC_SPC
     ),
 
-    [_FN] = LAYOUT(
-        KC_0, KC_1, KC_2, KC_5, \
-        C(KC_S), C(KC_COMMA), KC_L, KC_G, \
-        _______, C(KC_T), KC_LCTRL
+    [_MID] = LAYOUT(
+        C(KC_X), S(KC_RBRC), C(KC_G), KC_L, KC_P, \
+        _______, S(KC_LBRC), KC_D, KC_G, C(S(KC_Z)), \
+        _______, xxx, C(KC_S), KC_R
+    ),
+
+    [_LOWER] = LAYOUT(
+        KC_1, KC_2, KC_3, KC_4, KC_5, \
+        KC_6, KC_7, KC_8, KC_9, KC_0, \
+        KC_LSHIFT, KC_LALT, xxx, C(KC_SPC)
     )
 };
